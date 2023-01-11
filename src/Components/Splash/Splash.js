ae3a8a36347d60ecf85230cf13beb27ad003aa2c
Dani Bagley <bagley.a.danielle@gmail.com>
2023-01-11 13:47:23 -0700

    Splash css

--- src/Components/Splash/Splash.js
+++ src/Components/Splash/Splash.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Title from "/Users/danibagley/Turing/mod3/new-mixologist/src/Assets/Title.png";
 import "./Splash.css";
+import { Link } from "react-router-dom";
 
 const Splash = () => {
   return (
     <div className="splash">
       <img className="title" src={Title} />
-      <div className="start-button"></div>
+      {/* <div className="start-button">START</div> */}
+      <Link className="start-button">START</Link>
     </div>
   );
 };
