c6f384aadeee372748ed0729dfbc02cf66532c84
Dani Bagley <bagley.a.danielle@gmail.com>
2023-01-11 14:40:06 -0700

    Add question image

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Home.css";
 import NavBar from "../NavBar/NavBar";
+import Question from "/Users/danibagley/Turing/mod3/new-mixologist/src/Assets/Question.png";
 
 const Home = () => {
   return (
     <div>
       <NavBar />
+      <img className="question" src={Question} />
     </div>
   );
 };
