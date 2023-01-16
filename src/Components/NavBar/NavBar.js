6ca031f68c7f3fb960ffb1c9cfd5691617f7c630
Dani Bagley <bagley.a.danielle@gmail.com>
2023-01-16 11:29:08 -0700

    add change page state and function:

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,20 +1,55 @@
 import React from "react";
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import AppContext from "../App/AppContext";
 import "./NavBar.css";
 
 const NavBar = () => {
+  const [state, dispatch] = useContext(AppContext);
+
+  const changePage = (event) => {
+    dispatch({
+      type: "CHANGE_PAGE",
+      currentPage: event.target.innerText,
+    });
+  };
+
   return (
     <div className="navBar">
-      <NavLink to="/" className="navLink">
+      <NavLink
+        to="/"
+        className="navLink"
+        onClick={(event) => {
+          changePage(event);
+        }}
+      >
         HOME
       </NavLink>
-      <NavLink to="/home" className="navLink">
+      <NavLink
+        to="/home"
+        className="navLink"
+        onClick={(event) => {
+          changePage(event);
+        }}
+      >
         FILTER COCKTAILS
       </NavLink>
-      <NavLink to="/whats-my-drink" className="navLink">
+      <NavLink
+        to="/whats-my-drink"
+        className="navLink"
+        onClick={(event) => {
+          changePage(event);
+        }}
+      >
         WHAT'S MY DRINK?
       </NavLink>
-      <NavLink to="/random-drink" className="navLink">
+      <NavLink
+        to="/random-drink"
+        className="navLink"
+        onClick={(event) => {
+          changePage(event);
+        }}
+      >
         RANDOM COCKTAIL
       </NavLink>
     </div>
